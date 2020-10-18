b5ad94823a383b7bf10c9ca68ccd9ab131ffc9bf
malbannai <moudhi.albannai@gamil.com>
2020-10-18 19:01:05 +0300

    Story time

---
+++ variables/story.js
@@ -0,0 +1,7 @@
+let name = "Tom"
+let age = "5";
+let hoppy = "Sleeping"
+
+console.log("Mr./Mrs. "+ name +", in 33 years, you will be "+(age + 33)+" years old. By then you'll have decades of "+ hoppy +" practice!")
+
+console.log(`Testing for ${name}`)
